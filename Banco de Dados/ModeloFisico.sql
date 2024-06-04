8dc2872d8f13ec18f49cf1b76c185aa9d3a61744
enricoferrer <enrico.santana@sptech.school>
2024-06-04 14:03:04 -0300

    Banco com Inserts

--- Banco de Dados/ModeloFisico.sql
+++ Banco de Dados/ModeloFisico.sql
@@ -52,3 +52,30 @@ resultado varchar(45),
 primary key (id, fkUsuario),
 foreign key (fkUsuario) references usuario(id)
 );
+
+-- Inserindo a primeira linha
+INSERT INTO usuario (nome, sobrenome, email, senha)
+VALUES 
+('João', 'Silva', 'joao.silva@example.com', 'senha123'),
+('Maria', 'Souza', 'maria.souza@example.com', 'senha456'),
+('Pedro', 'Pereira', 'pedro.pereira@example.com', 'senha789'),
+('Ana', 'Oliveira', 'ana.oliveira@example.com', 'senha012'),
+('Carlos', 'Santos', 'carlos.santos@example.com', 'senha345'),
+('Bruna', 'Gomes', 'bruna.gomes@example.com', 'senha678'),
+('Diego', 'Ribeiro', 'diego.ribeiro@example.com', 'senha901'),
+('Clara', 'Alves', 'clara.alves@example.com', 'senha234'),
+('Fábio', 'Costa', 'fabio.costa@example.com', 'senha567'),
+('Fernanda', 'Monteiro', 'fernanda.monteiro@example.com', 'senha890');
+
+INSERT INTO quiz (fkUsuario, resultado)
+VALUES
+(1, "Muay Khao"),
+(2, "Muay Dan"),
+(3, "Muay Khao"),
+(4, "Muay Fimeu"),
+(5, "Muay Khao"),
+(6, "Muay Fimeu"),
+(7, "Muay Matt"),
+(8, "Muay Matt"),
+(9, "Muay Dan"),
+(10, "Muay Dan");
