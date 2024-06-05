2dbb5c83725c9827d62d1fa38418239908666927
enricoferrer <enrico.santana@sptech.school>
2024-06-04 22:17:19 -0300

    Edição do slide

--- Banco de Dados/ModeloFisico.sql
+++ Banco de Dados/ModeloFisico.sql
@@ -2,7 +2,6 @@ CREATE DATABASE MuayArt;
 
 USE MuayArt;
 
-
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
@@ -11,38 +10,6 @@ CREATE TABLE usuario (
 	senha VARCHAR(50)
 );
 
-/*CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT
-);
-
-/*create table aquario (
-em nossa regra de negócio, um aquario tem apenas um sensor 
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);*/
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-/*create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-*/
 select * from usuario;
 
 create table quiz (
@@ -53,9 +20,9 @@ primary key (id, fkUsuario),
 foreign key (fkUsuario) references usuario(id)
 );
 
--- Inserindo a primeira linha
 INSERT INTO usuario (nome, sobrenome, email, senha)
 VALUES 
+('Enrico', 'Ferrer', 'enrico@email.com', 'enrico123'),
 ('João', 'Silva', 'joao.silva@example.com', 'senha123'),
 ('Maria', 'Souza', 'maria.souza@example.com', 'senha456'),
 ('Pedro', 'Pereira', 'pedro.pereira@example.com', 'senha789'),
@@ -78,4 +45,5 @@ VALUES
 (7, "Muay Matt"),
 (8, "Muay Matt"),
 (9, "Muay Dan"),
-(10, "Muay Dan");
+(10, "Muay Dan"),
+(11, "Muay Fimeu");
